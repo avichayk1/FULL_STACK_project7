28a3f4f48ecde27fcdc58c9e1044eed8ed966c0a
avichay kadosh <avichayn@g.jct.ac.il>
2023-07-31 14:31:35 +0300

    i fix the picture

--- Server/models/managerDB.js
+++ Server/models/managerDB.js
@@ -1,21 +1,23 @@
-import { pool, con, pool1 } from "./connection.js";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url"; // Import fileURLToPath
+import { pool, con, pool1 } from './connection.js';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url'; // Import fileURLToPath
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 async function managerLogIn(user) {
-  console.log("in function managerLogIn");
+  console.log('in function managerLogIn');
   const sql = `SELECT * FROM managers where email="${user.email}" and password="${user.password}"`;
   const res = pool.query(sql);
   //   console.log(JSON.parse(res[0]));
   return res;
 }
 async function getFualtByType(req, res) {
-  console.log("in function getFualtByType");
+  console.log('in function getFualtByType');
   const id = req.params;
   const { property_id, status, type } = req.query;
 
-  const sql = `SELECT * FROM reports WHERE property_id=${property_id} AND status="${status}" AND type="${type}"`;
+  const sql = `SELECT * FROM reports WHERE property_id=${property_id} and status="${status}" and type="${type}"`;
 
   try {
     const result = await pool.query(sql);
@@ -26,9 +28,9 @@ async function getFualtByType(req, res) {
       if (report.image_path) {
         // Extract the file name from the path
         // Convert the relative path to an absolute path
-        const imageFilePath = path.join(__dirname, "..", report.image_path);
+        const imageFilePath = path.join(__dirname, '..', report.image_path);
         const imageBase64 = fs.readFileSync(imageFilePath, {
-          encoding: "base64",
+          encoding: 'base64',
         });
         report.image = `data:image/jpeg;base64,${imageBase64}`;
       }
@@ -36,41 +38,41 @@ async function getFualtByType(req, res) {
     console.log(reports);
     return res.status(200).json(reports);
   } catch (error) {
-    console.error("Error fetching reports:", error);
-    return res.status(500).json({ error: "Error fetching reports" });
+    console.error('Error fetching reports:', error);
+    return res.status(500).json({ error: 'Error fetching reports' });
   }
 }
 async function getAllProperties(req, res) {
-  console.log("in function getAllPropertiesNumbers");
+  console.log('in function getAllPropertiesNumbers');
   const sql = `SELECT id,city,address FROM properties where manager_id=${req.params.id}`;
   try {
     const resualt = await pool.query(sql);
     // console.log(resualt[0]);
     return res.status(200).json(resualt[0]);
   } catch (error) {
-    console.error("Error adding report:", error);
-    return res.status(500).json({ error: "Error adding report" });
+    console.error('Error adding report:', error);
+    return res.status(500).json({ error: 'Error adding report' });
   }
 }
 async function updateReportStatus(req, res) {
-  console.log("in function updateReportStatus");
+  console.log('in function updateReportStatus');
 
   const reportId = req.params.reportId;
   const { status } = req.body;
-  console.log(reportId, " ", status);
+  console.log(reportId, ' ', status);
   try {
     const updateQuery = `UPDATE reports SET status = ? WHERE fault_id = ?`;
     await pool.query(updateQuery, [status, reportId]);
 
-    res.status(200).json({ message: "Report status updated successfully." });
+    res.status(200).json({ message: 'Report status updated successfully.' });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Internal server error." });
+    res.status(500).json({ error: 'Internal server error.' });
   }
 }
 
 async function getManagerDetails(id) {
-  console.log("in function getManagerDetails");
+  console.log('in function getManagerDetails');
   const sql = `SELECT full_name, phone, email from managers WHERE manager_id = ${id}`;
   const response = pool.query(sql);
   return response;
